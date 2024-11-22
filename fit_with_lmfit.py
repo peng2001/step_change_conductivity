0c1ade75ab7c91f787166844df24654cb07a9ae8
peng2001 <yuangpeng2001@gmail.com>
2024-11-22 12:38:58 +0000

    change printing start time for step changes to setup file

--- fit_with_lmfit.py
+++ fit_with_lmfit.py
@@ -6,7 +6,7 @@ import toml
 from setup import *
 import math
 
-config_file = "config/75soc/config_1520.toml"
+config_file = "config/75soc/config_3540.toml"
 
 ##########################################################
 

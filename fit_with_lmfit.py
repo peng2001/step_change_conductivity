65e7eb7a4604af7738aa1363e2c427290c1885d8
peng2001 <yuangpeng2001@gmail.com>
2024-11-22 12:29:05 +0000

    calculate max errors

--- fit_with_lmfit.py
+++ fit_with_lmfit.py
@@ -6,15 +6,16 @@ import toml
 from setup import *
 import math
 
-config_file = "config/100soc/config_15to20.toml"
+config_file = "config/75soc/config_30to35.toml"
 
 ##########################################################
 
 with open(config_file, 'r') as f:
     inputs = toml.load(f)
 
-L = inputs["L"] # metres, equals 1/2 of cell thickness
-deltaT = inputs["deltaT"] # degrees C, magnitude of step change
+L_orig = inputs["L"] # metres, equals 1/2 of cell thickness
+deltaT_orig = inputs["deltaT"] # degrees C, magnitude of step change
+HeatfluxData_orig = HeatfluxData
 start_time = inputs["start_time"]+inputs["start_time_addition"] # start time elapsed to fit equation, seconds (the addition is determined manually, from the time diff when power is applied until temperature reaches steady state)
 end_time = inputs["end_time"] # end time elapsed to fit equation, seconds
 fitting_time_skip = inputs["fitting_time_skip"] # seconds, integer, ignore first few seconds because of overshoot
@@ -87,18 +88,7 @@ def calculate_fit_error(exp_time, exp_heatflux, conductivity,diffusivity,heat_fl
     avg_abs_relative_err = np.sum(np.abs(np.subtract(filtered_exp_heat,interpolated_fitted_heat_flux)))/np.sum(np.abs(filtered_exp_heat))
     print("Average absolute relative error of heat equation flux fit: "+str(100*avg_abs_relative_err)+" %")
 
-
-if __name__ == "__main__":
-    dq_dt = np.gradient(HeatfluxData.average_heatflux, HeatfluxData.time_elapsed) # Find time values where dq/dt > 100 time_values = t[dq_dt > 100]
-    jump_times = HeatfluxData.time_elapsed[dq_dt < -100]
-    filtered_times = []
-    previous_time = None
-    for time in jump_times:
-        if previous_time is None or (time - previous_time > 100):
-            filtered_times.append(time)
-            previous_time = time
-    print("Times where the step change starts")
-    print(str(filtered_times))
+def run_fitting():
     heat_flux_column = HeatfluxData.average_heatflux
     time_window = np.subtract([time for time in HeatfluxData.time_elapsed if start_time <= time <= end_time], start_time)
     heat_fluxes = [heat_flux_column[i] for i in range(len(HeatfluxData.time_elapsed)) if start_time <= HeatfluxData.time_elapsed[i] <= end_time]
@@ -125,10 +115,10 @@ if __name__ == "__main__":
     initial_loss_index = next(i for i, t in enumerate(HeatfluxData.time_elapsed) if t >= inputs["start_time"]) - 5 # 5 points before the start time to get initial loss
     prev_50_points = HeatfluxData.average_heatflux[(initial_loss_index-50):initial_loss_index] # get prev 100 points to get the average initial loss
     initial_loss_estimate = sum(prev_50_points) / len(prev_50_points)
-    print("Initial loss: "+str(initial_loss_estimate))
+    # print("Initial loss: "+str(initial_loss_estimate))
 
     fitted_initial, fitted_asymptote, fitted_tau = fit_exponential(time_window_for_fitting, heat_fluxes_for_fitting)
-    print("Final loss: "+str(fitted_asymptote))
+    # print("Final loss: "+str(fitted_asymptote))
     # print("HEat flux fitted to exponential:")
     # print(fitted_initial)
     # print(fitted_asymptote)
@@ -157,11 +147,82 @@ if __name__ == "__main__":
     diffusivityEminus5_error = result.params['diffusivityEminus5'].stderr
     diffusivity = (diffusivityEminus5)*10**(-5)
     diffusivity_error = (diffusivityEminus5_error)*10**(-5)
-    print("**Results**")
-    print("Conductivity: "+str(round_4_sig(conductivity))+" W/(m*K)")
-    print("Diffusivity: "+str(round_4_sig(diffusivity))+" m^2/s")
-    print("Conductivity stderr: "+str(conductivity_error)+" W/(m*K)")
-    print("Diffusivity stderr: "+str(diffusivity_error)+" m^2/s")
     # print("Heat flux offset: "+str(round_4_sig(heat_flux_offset))+" W/m^2")
     # graph_heat_vs_time(HeatfluxData.time_elapsed, HeatfluxData.average_heatflux)
-    graph_heat_vs_time_and_fitted_eqn(time_window, heat_fluxes, adjusted_heat_flux, conductivity,diffusivityEminus5,heat_flux_offset=0)
+    # graph_heat_vs_time_and_fitted_eqn(time_window, heat_fluxes, adjusted_heat_flux, conductivity,diffusivityEminus5,heat_flux_offset=0)
+    return conductivity, diffusivity, conductivity_error, diffusivity_error
+
+
+
+if __name__ == "__main__":
+    print("Fitting using measured parameters")
+    L = L_orig; deltaT = deltaT_orig; HeatfluxData = HeatfluxData_orig
+    conductivity, diffusivity, conductivity_error, diffusivity_error = run_fitting()
+    print("**Results**")
+    # print("Conductivity: "+str(round_4_sig(conductivity))+" W/(m*K)")
+    # print("Diffusivity: "+str(round_4_sig(diffusivity))+" m^2/s")
+    # print("Conductivity stderr: "+str(conductivity_error)+" W/(m*K)")
+    # print("Diffusivity stderr: "+str(diffusivity_error)+" m^2/s")
+
+    delta_ks = []
+    delta_alphas = []
+
+    # print("______________________________")
+    # print("Fitting using L + uncertainty")
+    L = L_orig+0.00001
+    conductivity_new, diffusivity_new, conductivity_error_new, diffusivity_error_new = run_fitting()
+    delta_k_1 = conductivity_new-conductivity
+    delta_alpha_1 = diffusivity_new-diffusivity
+    # print("delta k: "+str(conductivity_new-conductivity))
+    # print("delta alpha: "+str(diffusivity_new-diffusivity))
+    # print("Fitting using L - uncertainty")
+    L = L_orig-0.00001
+    conductivity_new, diffusivity_new, conductivity_error_new, diffusivity_error_new = run_fitting()
+    delta_k_2 = conductivity_new-conductivity
+    delta_alpha_2 = diffusivity_new-diffusivity
+    # print("delta k: "+str(conductivity_new-conductivity))
+    # print("delta alpha: "+str(diffusivity_new-diffusivity))
+    delta_ks.append(abs(max(abs(delta_k_1), abs(delta_k_2))))
+    delta_alphas.append(abs(max(abs(delta_alpha_1), abs(delta_alpha_2))))
+
+    # print("______________________________")
+    # print("Fitting using deltaT + uncertainty")
+    deltaT = deltaT_orig+0.01
+    conductivity_new, diffusivity_new, conductivity_error_new, diffusivity_error_new = run_fitting()
+    delta_k_1 = conductivity_new-conductivity
+    delta_alpha_1 = diffusivity_new-diffusivity
+    # print("delta k: "+str(conductivity_new-conductivity))
+    # print("delta alpha: "+str(diffusivity_new-diffusivity))
+    # print("Fitting using deltaT - uncertainty")
+    deltaT = deltaT_orig-0.01
+    conductivity_new, diffusivity_new, conductivity_error_new, diffusivity_error_new = run_fitting()
+    delta_k_2 = conductivity_new-conductivity
+    # delta_alpha_2 = diffusivity_new-diffusivity
+    # print("delta k: "+str(conductivity_new-conductivity))
+    # print("delta alpha: "+str(diffusivity_new-diffusivity))
+    delta_ks.append(abs(max(abs(delta_k_1), abs(delta_k_2))))
+    delta_alphas.append(abs(max(abs(delta_alpha_1), abs(delta_alpha_2))))
+
+    # print("______________________________")
+    # print("Fitting using Heat flux + uncertainty and start, - uncertainty at end")
+    HeatfluxData.average_heatflux = np.multiply(HeatfluxData_orig.average_heatflux, np.linspace(1.03, 0.97, len(HeatfluxData_orig.average_heatflux)))
+    conductivity_new, diffusivity_new, conductivity_error_new, diffusivity_error_new = run_fitting()
+    delta_k_1 = conductivity_new-conductivity
+    delta_alpha_1 = diffusivity_new-diffusivity
+    # print("delta k: "+str(conductivity_new-conductivity))
+    # print("delta alpha: "+str(diffusivity_new-diffusivity))
+    # print("Fitting using Heat flux - uncertainty and start, + uncertainty at end")
+    HeatfluxData.average_heatflux = np.multiply(HeatfluxData_orig.average_heatflux, np.linspace(0.97, 1.03, len(HeatfluxData_orig.average_heatflux)))
+    conductivity_new, diffusivity_new, conductivity_error_new, diffusivity_error_new = run_fitting()
+    delta_k_2 = conductivity_new-conductivity
+    delta_alpha_2 = diffusivity_new-diffusivity
+    # print("delta k: "+str(conductivity_new-conductivity))
+    # print("delta alpha: "+str(diffusivity_new-diffusivity))
+    delta_ks.append(abs(max(abs(delta_k_1), abs(delta_k_2))))
+    delta_alphas.append(abs(max(abs(delta_alpha_1), abs(delta_alpha_2))))
+    
+    sum_of_errors_k = np.sum(delta_ks+conductivity_error)
+    sum_of_errors_alpha = np.sum(delta_alphas+diffusivity_error)
+    print("Conductivity: "+str(round_4_sig(conductivity))+" W/(m*K)"+" +- "+str(round_4_sig(sum_of_errors_k)))
+    print("Diffusivity: "+str(round_4_sig(diffusivity))+" m^2/s"+" +-" +str(round_4_sig(sum_of_errors_alpha)))
+    

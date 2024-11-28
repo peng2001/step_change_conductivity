f33e564bfd80b6b3488d8a23c72e90168689855a
peng2001 <yuangpeng2001@gmail.com>
2024-11-28 11:48:49 +0000

    25soc data

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-directory = 'data/in-plane-thermal-cond/50soc'
-config_file = "config/50soc/config_1520.toml"
+directory = 'data/in-plane-thermal-cond/25soc_3040'
+config_file = "config/25soc/config_3035.toml"
 
 import os
 import pandas as pd
